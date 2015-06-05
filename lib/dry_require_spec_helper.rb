87b41e72b9e24893acc39d71c2953074aceea255
Koichi ITO <koic.ito@gmail.com>
2015-06-05 20:28:20 +0900

    to be executable command.

--- lib/dry_require_spec_helper.rb
+++ lib/dry_require_spec_helper.rb
@@ -1,11 +1,1 @@
-File.open('.rspec', 'a') {|file| file.write("--require spec_helper\n") }
-
-Dir['./spec/**/*.rb'].each do |path|
-  source = File.read(path)
-
-  next unless /require +('spec_helper'|"spec_helper")\n*/ === source
-
-  source.gsub!($&, '')
-
-  File.open(path, 'w+') {|f| f.write(source) }
-end
+module DryRequireSpecHelper; end
